a547d23bb84162ec549abdb054b15e72b88bcfcf
Lefreteur <ethan.lefreteur@estiam.com>
2025-09-24 11:58:26 +0200

    Update css ImageTextBlock

--- src/components/ImageTextBlock.vue
+++ src/components/ImageTextBlock.vue
@@ -12,15 +12,15 @@ import TextBlock from './TextBlock.vue';
         <img :src="Image" />
         <TextBlock :Title="Title" :Text="Text" />
     </div>
-
-
 </template>
 
 <style scoped>
-
 .ImageTextBlock{
     display: flex;
     flex-direction: row;
-    max-width: 80vw;
+    max-width: 70vw;
+}
+.TextBlock {
+    padding: 30px;
 }
 </style>
