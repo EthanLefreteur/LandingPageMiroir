37c0900d7e47778c1e85e46ee27bcd02f8b49785
Lefreteur <ethan.lefreteur@estiam.com>
2025-09-24 11:59:23 +0200

    Ajout espace en bas

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ function scrollToAnchor(id) {
     <TextBlock Title="Le Miroir du futur"
       Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
     <Space space="100px" />
-    
+
     <ImageTextBlock Title="Bien plus qu'un miroir"
       Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       :Image="image"
@@ -53,7 +53,7 @@ function scrollToAnchor(id) {
     <Space space="100px" />
     <TextBlock Title="Contactez nous !"
       Text="Lorem ipsum dolor sit amet !" id="contact"/>
-    
+    <Space space="100px" />
   </main>
 </template>
 
